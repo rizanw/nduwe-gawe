4830ffd87264d42c1de6eb0e9a59b9645c54bbd1
Rizky Andre Wibisono <rzkandre@gmail.com>
2019-12-02 03:43:59 +0800

    mobile testing: download by id fixing

--- app/Http/Controllers/UndanganWatermarkerController.php
+++ app/Http/Controllers/UndanganWatermarkerController.php
@@ -161,7 +161,7 @@ class UndanganWatermarkerController extends Controller
         });
 
         //tempat
-        $img->text($undanganDetail->tempat, 320, 620, function ($font) {
+        $img->text($undanganDetail->tempat_resepsi, 320, 620, function ($font) {
             $font->file(base_path('public/fonts/Kastella.ttf'));
             $font->size(22);
             $font->color('#777');
@@ -169,7 +169,7 @@ class UndanganWatermarkerController extends Controller
             $font->valign('top');
         });
         //alamat
-        $img->text($undanganDetail->alamat, 320, 690, function ($font) {
+        $img->text($undanganDetail->alamat_resepsi, 320, 690, function ($font) {
             $font->file(base_path('public/fonts/Kastella.ttf'));
             $font->size(22);
             $font->color('#777');
